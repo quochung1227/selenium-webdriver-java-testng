3baa8c0075d0e100e6c4723a6a09563f313ff475
NGUYEN QUOC HUNG <52438765+quochung1227@users.noreply.github.com>
2020-11-26 09:35:28 +0700

    Update Topic_03_selenium_webdriver.java
     Bai tap Xpath/Css

--- src/package1/Topic_03_selenium_webdriver.java
+++ src/package1/Topic_03_selenium_webdriver.java
@@ -1,34 +1,78 @@
 package package1;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
+
 
 public class Topic_03_selenium_webdriver {
-	WebDriver driver;
-	@BeforeClass
-	public void beforeClass() {
-		driver = new FirefoxDriver();
-		driver.get("https://zingnews.vn/");
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-	}
-
-	@Test
-	public void f() {
-		// Verify title
-		String homePageTitle = driver.getTitle();
-		Assert.assertEquals(homePageTitle, "Zing -  tin uy tín, hình ảnh ấn tượng - ZINGNEWS.VN");
-	}
-
-	@AfterClass
-	public void afterClass() {
-		driver.quit();
-	}
+WebDriver driver;
+@Test
+public void TC_01() throws InterruptedException {
+    // 1. Mở trinh duyệt
+    driver=new FirefoxDriver();
+    // nhạp vao trang 
+    driver.get("http://live.demoguru99.com/");
+    // Click vào "My Account" để dăng nhập 
+    //Theo X-path 
+    driver.findElement(By.xpath(".//*[@id='top']/body/div/div/div[3]/div/div[4]/ul/li[1]/a")).click();
+    // Click LOGIN button
+    driver.findElement(By.id("send2")).click();
+    // Kiem tra require co xuat hien ko 
+    driver.findElement(By.id("advice-required-entry-email")).isDisplayed();
+    driver.findElement(By.id("advice-required-entry-pass")).isDisplayed();
+    Thread.sleep(4000);
+    driver.close();
+}
+@Test
+public void TC_02() throws InterruptedException {
+	driver=new FirefoxDriver();
+	driver.get("http://live.demoguru99.com/");
+	driver.findElement(By.xpath(".//*[@id='top']/body/div/div/div[3]/div/div[4]/ul/li[1]/a")).click();
+	driver.findElement(By.id("email")).sendKeys("123434234@12312.123123");
+	driver.findElement(By.id("pass")).sendKeys("123456");
+	driver.findElement(By.id("send2")).click();
+	driver.findElement(By.id("advice-validate-email-email")).isDisplayed();
+	Thread.sleep(4000);
+	driver.close();
+}
+@Test
+public void TC_03() throws InterruptedException {
+	driver=new FirefoxDriver();
+	driver.get("http://live.demoguru99.com/");
+	driver.findElement(By.xpath(".//*[@id='top']/body/div/div/div[3]/div/div[4]/ul/li[1]/a")).click();
+	driver.findElement(By.id("email")).sendKeys("automation@gmail.com");
+	driver.findElement(By.id("pass")).sendKeys("123");
+	driver.findElement(By.id("send2")).click();
+	driver.findElement(By.id("advice-validate-password-pass")).isDisplayed();
+	Thread.sleep(5000);
+	driver.close();
+}
+@Test
+public void TC_04() throws InterruptedException {
+	driver=new FirefoxDriver();
+	driver.get("http://live.demoguru99.com/");
+	driver.findElement(By.xpath(".//*[@id='top']/body/div/div/div[3]/div/div[4]/ul/li[1]/a")).click();
+	driver.findElement(By.id("email")).sendKeys("automation@gmail.com");
+	driver.findElement(By.id("pass")).sendKeys("123123123");
+	driver.findElement(By.id("send2")).click();
+	driver.findElement(By.xpath(".//*[@id='top']/body/div/div/div[2]/div/div/div/ul/li/ul/li/span")).isDisplayed();
+	Thread.sleep(5000);
+	driver.close();
+}
+@Test
+public void TC_05() {
+	driver=new FirefoxDriver();
+	driver.get("http://live.demoguru99.com/");
+	driver.findElement(By.xpath(".//*[@id='top']/body/div/div/div[3]/div/div[4]/ul/li[1]/a")).click();
+	driver.findElement(By.xpath("//a[@title=\"Create an Account\"]")).click();
+	driver.findElement(By.id("firstname")).sendKeys("Hung");
+	driver.findElement(By.id("middlename")).sendKeys("Quoc");
+	driver.findElement(By.id("lastname")).sendKeys("Nguyen");
+	driver.findElement(By.id("email_address")).sendKeys("nguyenquochung2712@gmail.com");
+	driver.findElement(By.id("password")).sendKeys("123456");
+	driver.findElement(By.id("confirmation")).sendKeys("123456");
+	
+}
 }
